bfb51e98fc1ec4da7ba924fa60d62243e46a0821
stryku <stryku2393@gmail.com>
2016-10-13 19:57:07 +0200

    introduce array

---
+++ include/cexpr/array/array.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace cexpr
+{
+    template <typename T, size_t N>
+    struct array
+    {
+        constexpr T& operator[](size_t i)
+        {
+            return arr[i];
+        }
+
+        constexpr const T& operator[](size_t i) const
+        {
+            return arr[i];
+        }
+
+        constexpr T* begin()
+        {
+            return arr;
+        }
+
+        constexpr const T* cbegin() const
+        {
+            return arr;
+        }
+
+        constexpr T* end()
+        {
+            return arr + N;
+        }
+        constexpr const T* cend() const
+        {
+            return arr + N;
+        }
+
+        T arr[N];
+    };
+}
